d325c6c8abd12ef6814550d6e2fa61f1838a71f3
Crilum <91354257+Crilum@users.noreply.github.com>
2023-05-02 14:04:32 -0600

    Init commit

---
+++ docusaurus.config.js
@@ -0,0 +1,117 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Botshard Docs',
+  tagline: 'Getting started with Botshard',
+  favicon: 'img/botshardlogo.png',
+
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-test-site.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'Botshard', // Usually your GitHub org/user name.
+  projectName: 'documentation', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'Home',
+        logo: {
+          alt: 'Botshard logo',
+          src: 'img/botshardlogo.png',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Getting started',
+            href: 'Getting-Started',
+          },
+          {
+            href: 'https://github.com/BotShard/documentation/',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Getting Started',
+                to: 'Getting-Started',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Discord',
+                href: 'https://discord.gg/fCa4tQg6nd',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/BotShard/documentation',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Botshard. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
---
+++ src/pages/Getting-Started.md
@@ -0,0 +1,34 @@
+---
+title: Getting started with Botshard
+---
+
+# Getting Started
+
+### 1. Create an account
+
+Start by creating a [Botshard](https://botshard.com/) account.
+    
+[![Create account](../../static/img/Create-Account.png)](../../static/img/Create-Account.png)
+[![Filled account forms](../../static/img/Create-Account-filled.png)](../../static/img/Create-Account-filled.png)
+
+
+:::tip Tip
+
+If you already have an account, you can skip this step.
+
+:::
+
+
+### 2. Create a server
+
+Once you have an account, go to the [dashboard](dashboard.botshard.com), and click on the `Servers` tab.
+
+[![Servers tab](../../static/img/Dashboard-Servers.png)](../../static/img/Dashboard-Servers.png)
+
+:::tip Tip
+On this page you can create servers, and see all the servers you have on Botshard.
+:::
+
+Click on the `Create Server` button.
+
+[![Create Server button](../../static/img/Create-Server.png)](../../static/img/Create-Server.png)
